dc2bc057406b19de4bc95edc46ac545b1da730d9
Coordinacion de Desarrollo <89142648+CDSysdatec01@users.noreply.github.com>
2023-06-02 08:53:06 -0500

    Ajustes para adicion de servicios
    Se agrega el manejo del login por medio del servicio y la interaccion

--- src/Componentes/Registro/Login/Login.js
+++ src/Componentes/Registro/Login/Login.js
@@ -12,6 +12,7 @@ import Sade from "../images/LogoSprime.png";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Select from "react-select";
+import { Modal } from "react-bootstrap";
 
 const SelectBox = ({ url, valueKey, labelKey }) => {
   const [options, setOptions] = useState([]);
@@ -40,6 +41,30 @@ const SelectBox = ({ url, valueKey, labelKey }) => {
 };
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [showModal, setShowModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+
+  const handleLogin = () => {
+    axios
+      .post("https://sadecv.sysdatec.com/Security/Users/Login", {
+        username,
+        password,
+      })
+      .then((response) => {
+        // Maneja la respuesta exitosa del inicio de sesión
+        setModalMessage("Inicio de sesión exitoso");
+        setShowModal(true);
+      })
+      .catch((error) => {
+        // Maneja cualquier error que ocurra durante el inicio de sesión
+        setModalMessage("Error en el inicio de sesión");
+        setShowModal(true);
+      });
+  };
+
   return (
     <div className="d-flex justify-content-center">
       <Card className="w-50 p-3 mt-5" decoration="top" decorationColor="indigo">
@@ -61,18 +86,26 @@ function Login() {
             />
             <Title className="my-2">Metodo de Autenticacion:</Title>
             <TextInput value="TRADICIONAL" placeholder="" readOnly />
-            <Title className="my-2">Usuario:</Title>
-            <TextInput placeholder="" />
-            <Title className="my-2">Contraseña:</Title>
-            <TextInput placeholder="" type="password" />
 
-            <Button
-              className="my-3 w-100"
-              size="xl"
-              onClick={() => console.log("clicked")}
-            >
+            <Title className="my-2">Usuario:</Title>
+            <TextInput
+              placeholder=""
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+
+            <Title className="my-2">Contraseña:</Title>
+            <TextInput
+              placeholder=""
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <Button className="my-3 w-100" size="xl" onClick={handleLogin}>
               Iniciar Sesión
             </Button>
+
             <Flex justifyContent="center" className="space-x-2 ">
               <Button
                 size="lg"
@@ -81,6 +114,16 @@ function Login() {
               >
                 Recordar Contraseña
               </Button>
+
+              <Modal show={showModal} onHide={() => setShowModal(false)}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Resultado del inicio de sesión</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>{modalMessage}</Modal.Body>
+                <Modal.Footer>
+                  <Button onClick={() => setShowModal(false)}>Cerrar</Button>
+                </Modal.Footer>
+              </Modal>
             </Flex>
           </Col>
         </Grid>
